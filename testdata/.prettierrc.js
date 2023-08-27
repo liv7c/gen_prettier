55b57fc7261328bddd132abf13eacc11acd27256
Olivia Coumans <livie7c@gmail.com>
2023-08-27 21:38:14 +0200

    feat: add config generator func

---
+++ testdata/.prettierrc.js
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+    arrowParens: 'always',
+    bracketSpacing: false,
+    printWidth: 80,
+    semi: false,
+    singleQuote: true,
+    tabWidth: 4,
+    trailingComma: 'es5',
+    useTabs: false,
+}
+
+module.exports = config
